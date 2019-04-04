0ded6591b17db66e492babfd90bad4aadd90d8fa
boyd1915 <nanbo@baiseng.com>
2019-04-04 18:46:04 +0800

    nacos 基础配置

---
+++ nacos-discovery-consumer/src/main/java/com/tomato/nacosdiscoveryconsumer/NacosDiscoveryConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.nacosdiscoveryconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosDiscoveryConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDiscoveryConsumerApplication.class, args);
+    }
+
+}
---
+++ nacos-discovery-provider/src/main/java/com/tomato/nacosdiscoveryprovider/NacosDiscoveryProviderApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.nacosdiscoveryprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosDiscoveryProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDiscoveryProviderApplication.class, args);
+    }
+
+}
